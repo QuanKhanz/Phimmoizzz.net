fb063c6dbae6a18ffdb4a2125fc01bab32752a81
QuanKhanz <iamhoangkhang@icloud.com>
2023-08-27 21:39:00 +0700

    Completed navigation bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        barlow:['"Barlow Condensed"', 'sans-serif']
+      },
+      colors:{
+        primary: '#16405B'
+      }
+    },
   },
   plugins: [],
 }
